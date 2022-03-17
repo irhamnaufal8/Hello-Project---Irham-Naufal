8991e8a1f39e134f33976bef2777badea0100fe4
Irham Naufal <inaufal77@gmail.com>
2022-03-17 12:08:33 +0700

    adding xcode projects

---
+++ Hello-Project/Hello-Project/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Hello-Project
+//
+//  Created by Garry on 17/03/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, project!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Hello-Project/Hello-Project/Hello_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hello_ProjectApp.swift
+//  Hello-Project
+//
+//  Created by Garry on 17/03/22.
+//
+
+import SwiftUI
+
+@main
+struct Hello_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
